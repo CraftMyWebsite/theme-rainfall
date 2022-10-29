6b669ec2116b04f5fda82453ac043ff3f9211cec
Léo <69589034+Zomblard@users.noreply.github.com>
2022-10-29 15:37:22 +0200

    [CREATE] projet init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './dev/**/*.{php,html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui")
+  ],
+}

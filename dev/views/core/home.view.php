3cf0c3de43b088cc1dc170ac5062304dba372976
Léo <69589034+Zomblard@users.noreply.github.com>
2022-10-29 20:35:41 +0200

    [ADD] default page

---
+++ dev/views/core/home.view.php
@@ -0,0 +1,38 @@
+<div style="background-image: url('/dev/img/bg.jpg');" class="absolute img_bg w-screen h-screen -z-10">
+    <div class="layer flex">
+        <div class="mx-auto my-auto lg:mt-28 text-center text-white">
+            <img class="mx-auto mb-4 lg:mb-12 w-56" src="/dev/img/logo.png" alt="logo.svg introuvable !">
+            <div class="font-bold text-5xl lg:text-8xl">RainFall</div>
+            <div class="text-sm lg:text-lg">Une super description pour mon site !</div>
+            <div class="px-4 py-2 w-full sm:w-auto mt-4 text-4xl">
+                <div class="flex-wrap inline-flex space-x-6 lg:space-x-16">
+                    <!--TODO : Possibilité de le désactiver - Editer le lien-->
+                    <a href="#" class="hover:text-blue-600">
+                        <i class=" fa-brands fa-facebook"></i>
+                    </a>
+                    <!--TODO : Possibilité de le désactiver - Editer le lien dans les config du theme-->
+                    <a href="#" class="hover:text-blue-600">
+                        <i class=" fa-brands fa-square-twitter"></i>
+                    </a>
+                    <!--TODO : Possibilité de le désactiver - Editer le lien dans les config du theme-->
+                    <a href="#" class="hover:text-blue-600">
+                        <i class=" fa-brands fa-instagram"></i>
+                    </a>
+                    <!--TODO : Possibilité de le désactiver - Editer le lien dans les config du theme-->
+                    <a href="#" class="hover:text-blue-600">
+                        <i class=" fa-brands fa-discord"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="text-gray-500 mt-20 lg:mt-60">
+                <div class="p-2 text-xs lg:text-base">
+                    <p>Copyright © 2022<br>Site créer par <b><a href="https://craftmywebsite.fr/" target="_blank">CraftMyWebsite</a></b> pour <!--TODO : Récupération du titre du site--></p>
+                    <p class="hidden">Credit thème : Z0mblard</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
---
+++ dev/views/template.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once("includes/head.inc.php");
+include_once("includes/header.inc.php");
+
+
+?>
+
+<!-- En prod (Appeler le content ici)-->
+<?php
+include_once("users/login.view.php");
+?>
+
+
+<?php
+//En prod : View::loadInclude($includes, "afterScript");
+include_once("includes/footer.inc.php");
+?>

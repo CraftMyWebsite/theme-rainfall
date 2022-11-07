cd8e2fac459ab77f4d3266b5404ff24e66d01d5d
Léo <69589034+Zomblard@users.noreply.github.com>
2022-11-07 17:38:21 +0100

    [ADD] news pages

---
+++ dev/views/news/list.view.php
@@ -0,0 +1,59 @@
+<div style="background-color: #18202E !important;" class="w-full pt-14 pb-4">
+    <div class="text-center pt-4 font-bold text-4xl border-t border-gray-500">Actualités</div>
+</div>
+<div class="custom-shape-divider-top-1667065406">
+    <svg width="100%" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" class="h-2 lg:h-10" style="background-color: #1e293b !important; color: #18202E;">
+        <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z" opacity=".25" class="shape-fill"></path>
+        <path d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z" opacity=".5" class="shape-fill"></path>
+        <path d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z" class="shape-fill"></path>
+    </svg>
+</div>
+
+<section class="px-4 lg:px-16 py-6">
+
+
+    <div class="lg:grid lg:grid-cols-5 gap-6">
+        <div style="background-color: #18202E !important;" class="container mx-auto rounded-md shadow-lg p-4 h-fit">
+            <div class="flex flex-no-wrap justify-center items-center py-4 -mb-6">
+                <div class="px-10 w-auto">
+                    <h2 class="font-semibold text-2xl uppercase">Widgets</h2>
+                </div>
+            </div>
+            <div class="font-medium text-gray-500 mt-6 cursor-default">Discord </div>
+            <iframe class="w-full" src="https://discord.com/widget?id=1011276808341180426&theme=dark" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+        </div>
+
+
+
+
+    <div class="lg:grid lg:grid-cols-2 col-span-4 gap-6 mt-4 lg:mt-0 h-fit">
+        <div style="background-color: #18202E !important;" class="mb-4 lg:mb-0 flex flex-wrap h-full overflow-hidden rounded-md shadow-lg">
+            <div class="w-full md:w-5/12 lg:w-full xl:w-5/12">
+                <div class="h-full relative">
+                    <img src="https://top-mmo.fr/wp-content/uploads/2022/05/A7zC3bvSNxWGyaP8jSuz2W-1200-80.jpg" class="h-full object-cover w-full" alt="..." width="240" height="260"/>
+                    <div class="absolute bg-blue-600 font-semibold leading-tight left-0 ml-3 mt-3 px-3 py-2 text-center top-0">
+                        <div class="text-sm">Zomb</div>
+                    </div>
+                </div>
+            </div>
+            <div class="px-8 py-6 w-full md:w-7/12 lg:w-full xl:w-7/12">
+                <div class="mb-3">
+                    <div class="bg-gray-300 font-medium inline-block px-3 py-1 rounded-sm text-xs uppercase">26/10/2022 à 13:11:22</div>
+                </div>
+                <h3 class="font-bold leading-tight mb-3 text-xl">
+                    <a href="#" class="hover:text-blue-600">I 💞 Front !</a></h3>
+                <p class="mb-3">Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse duis deserunt mollit....</p>
+                <div class="mt-6 flex justify-between">
+                    <a href="#" class="font-bold hover:text-blue-700 text-sm">Lire la suite <i class="fa-solid fa-caret-right"></i></a>
+                    <div class="cursor-pointer">
+                        <span class="text-base">0 <i class="fa-regular fa-heart"></i></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    </div>
+
+</section>

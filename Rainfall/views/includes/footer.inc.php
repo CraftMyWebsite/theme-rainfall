b3388a9b847d807510db76e9bb095b68f26b373a
Léo <69589034+Zomblard@users.noreply.github.com>
2023-06-25 21:07:52 +0200

    [RM]

--- Rainfall/views/includes/footer.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-use CMW\Utils\Utils;
-?>
-<div class="text-gray-600">
-    <div class="p-2 text-xs lg:text-base">
-        <p>Copyright © 2022<br>Site créer par <b><a href="https://craftmywebsite.fr/" target="_blank">CraftMyWebsite</a></b> pour <b><?= Utils::getSiteName()?></b></p>
-        <p class="hidden">Credit thème : Z0mblard</p>
-    </div>
-</div>

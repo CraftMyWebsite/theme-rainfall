6e610873bf9550d3d486a3a4a42eb931b9914fa5
Léo <69589034+Zomblard@users.noreply.github.com>
2023-06-27 23:00:31 +0200

    [ADD] configs

--- Config/config.settings.php
+++ Config/config.settings.php
@@ -10,9 +10,95 @@ return [
     "global_no_register_message" => "Nous somme désolé mais les inscriptions sont pour le moment désactiver.",
 
     /* - - - - - - - -
-            - - HOME PAGE - -
-         - - - - - - - - - */
+     - - WIDGETS - -
+    - - - - - - - - - */
+    "widget_use_page" => "1",
+    "widget_use_cgu" => "1",
+    "widget_use_cgv" => "1",
+    "widget_use_news_list" => "1",
+
+    /* - - - - - - - -
+    - - HOME PAGE - -
+    - - - - - - - - - */
     "home_background" => "Config/Default/Img/bg.jpg",
+
+    /* - - - - - - - -
+       - - NEWS - -
+    - - - - - - - - - */
+    "news_title" => "Nouveautés",
+    "news_description" => "Les dernières actus !",
+    "news_page_title" => "Archives des nouveautés",
+    "news_page_number_display" => "20",
+
+    /* - - - - - - - -
+       - - F.A.Q - -
+    - - - - - - - - - */
+    "faq_title" => "FAQ",
+    "faq_description" => "Retrouvez toutes les réponses à vos questions",
+    "faq_page_title" => "F.A.Q",
+    "faq_question_title" => "Une question ?",
+    "faq_answer_title" => "Des réponses",
+    "faq_display_autor" => "1",
+    "faq_display_form" => "1",
+
+    /* - - - - - - - -
+       - - VOTES - -
+    - - - - - - - - - */
+    "vote_title" => "Votes",
+    "vote_description" => "Votez pour le serveur et gagnez des récompenses uniques !",
+    "votes_page_title" => "Voter",
+    "votes_participate_title" => "Participer",
+    "votes_top_10_title" => "Top 10 du mois",
+    "votes_top_10_global_title" => "Top 10 global",
+    "votes_display_global" => "1",
+
+    /* - - - - - - - -
+       - - WIKI - -
+    - - - - - - - - - */
+    "wiki_title" => "Wiki",
+    "wiki_description" => "Découvrez notre wiki !",
+    "wiki_page_title" => "Wiki",
+    "wiki_menu_title" => "Navigation",
+    "wiki_display_categorie_icon" => "1",
+    "wiki_display_article_categorie_icon" => "1",
+    "wiki_display_article_icon" => "1",
+    "wiki_display_creation_date" => "1",
+    "wiki_display_edit_date" => "1",
+    "wiki_display_autor" => "1",
+
+    /* - - - - - - - -
+       - - FORUM - -
+    - - - - - - - - - */
+    "forum_title" => "Forum",
+    "forum_description" => "Parcourez notre forum",
+    "forum_use_widgets" => "1",
+    "forum_widgets_show_stats" => "1",
+    "forum_widgets_title_stats" => "Stats du forum",
+    "forum_widgets_show_member" => "1",
+    "forum_widgets_text_member" => "Membres totaux :",
+    "forum_widgets_show_messages" => "1",
+    "forum_widgets_text_messages" => "Messages totaux :",
+    "forum_widgets_show_topics" => "1",
+    "forum_widgets_text_topics" => "Nombres de topics :",
+    "forum_widgets_show_discord" => "1",
+    "forum_widgets_content_id" => "(Paramètres serveur > Widget > ID serveur)",
+    "forum_widgets_show_custom" => "1",
+    "forum_widgets_custom_title" => "Widget personnaliser",
+    "forum_widgets_custom_text" => "<p>Bonjour !</p>",
+
+    "forum_breadcrumb_home" => "Accueil",
+    "forum_btn_create_topic_icon" => "fa-solid fa-pen-to-square",
+    "forum_btn_create_topic" => "Créer un topic",
+
+    "forum_sub_forum" => "Sous-Forums",
+    "forum_topics" => "Topics",
+    "forum_message" => "Messages",
+    "forum_last_message" => "Dernier messages",
+    "forum_display" => "Affichages",
+    "forum_response" => "Réponses",
+
+    "forum_nobody_send_message_img" => "Config/Default/Img/Forum/nobody.png",
+    "forum_nobody_send_message_text" => "Aucun message",
 
     /* - - - - - - - -
        - - FOOTER - -

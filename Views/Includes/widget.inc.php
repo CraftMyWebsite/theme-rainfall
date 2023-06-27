d4567ce22902e684520b762435a4aa430e4a4b25
Léo <69589034+Zomblard@users.noreply.github.com>
2023-06-27 22:39:32 +0200

    [ADD] Widget

--- Views/Includes/widget.inc.php
+++ Views/Includes/widget.inc.php
@@ -1,1 +1,11 @@
-<?php
+<div style="background-color: #18202E !important;" class="container mx-auto rounded-md shadow-lg p-4 h-fit">
+    <div class="flex flex-no-wrap justify-center items-center py-4 -mb-6">
+        <div class="px-10 w-auto">
+            <h2 class="font-semibold text-2xl uppercase">Widgets</h2>
+        </div>
+    </div>
+    <div class="font-medium text-gray-500 mt-6 cursor-default">Discord</div>
+    <iframe class="w-full" src="https://discord.com/widget?id=1011276808341180426&theme=dark" height="500"
+            allowtransparency="true" frameborder="0"
+            sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+</div>

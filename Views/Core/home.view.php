f494ecdef3d91d973da8834c98aaa57eb2b0ccb5
Léo <69589034+Zomblard@users.noreply.github.com>
2023-07-15 21:21:49 +0200

    [Fix] Home title

--- Views/Core/home.view.php
+++ Views/Core/home.view.php
@@ -16,7 +16,7 @@ $description = Website::getWebsiteDescription();
     <div class="layer flex">
         <div class="mx-auto my-auto lg:mt-28 text-center text-white">
             <img class="mx-auto mb-4 lg:mb-12 w-56" src="<?= ThemeModel::fetchImageLink("header_img_logo") ?>" alt="logo introuvable !">
-            <div class="font-bold text-5xl lg:text-8xl">RainFall</div>
+            <div class="font-bold text-5xl lg:text-8xl mb-4"><?= Website::getWebsiteName() ?></div>
             <div class="text-sm lg:text-lg">Une super description pour mon site !</div>
 
             <div class="px-4 py-2 w-full sm:w-auto mt-4 text-4xl">

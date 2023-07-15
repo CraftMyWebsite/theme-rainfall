0d76ffed17116afdaa56643b5bcb872988638c26
Léo <69589034+Zomblard@users.noreply.github.com>
2023-07-15 19:17:58 +0200

    [Add] Captcha forgot password

--- Views/Users/forgot_password.view.php
+++ Views/Users/forgot_password.view.php
@@ -27,6 +27,7 @@ use CMW\Utils\Website;
                     <label for="email" class="block mb-2 text-sm font-medium">Mail</label>
                     <input name="mail" type="email" class="bg-gray-800 border border-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="mail@craftmywebsite.fr" required>
                 </div>
+                <?php SecurityController::getPublicData(); ?>
                 <button type="submit" class="w-full bg-blue-900 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Connexion</button>
             </form>
 
